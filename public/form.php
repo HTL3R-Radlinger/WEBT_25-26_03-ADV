33a0f9696e70a1d5f20fb8935e322237b77fdf0e
AlexBa16 <alexbachinger56+GitHub@gmail.com>
2025-12-13 19:43:54 +0100

    US8

--- public/form.php
+++ public/form.php
@@ -7,13 +7,26 @@ use Radlinger\Mealplan\View\TemplateEngine;
 $apiLink = "http://localhost:8080/api.php?mealplanID=";
 
 $data = [
-    'title'=>"Form",
-    'header'=>"Generate Meal QR Code",
+    'title' => "Form",
+    'header' => "Generate Meal QR Code",
+    'action' => htmlspecialchars($_SERVER['PHP_SELF']),
     'qr_result' => "",
     'error' => ""
 ];
 
 //QrCodeBuilder::generate('http://localhost:8080/api.php?mealplanID=' . $plan->id, 'MealPlan Nr.: ' . $plan->id)->getDataUri()
 
+//echo TemplateEngine::render('../templates/form.html', $data);
 
-echo TemplateEngine::render('../templates/from.html', $data);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $mealId = trim($_POST['meal_id'] ?? '');
+
+    if (!preg_match('/^\d+$/', $mealId)) {
+        $data['error'] = '<p style="color:red">Invalid Meal ID format. (Needs to be a number)</p>';
+    } else {
+        $qr = QrCodeBuilder::generate('http://localhost:8080/api.php?mealplanID=' . $mealId, 'MealPlan Nr.: ' . $mealId)->getDataUri();
+        $data['qr_result'] = '<img src="' . $qr . '" alt="Generated QR Code">';
+    }
+}
+
+echo TemplateEngine::render('../templates/form.html', $data);
